39520dd97ccdaa1c60399c1fc926760bbee45d25
Sonja Peschke <sonjape91@gmail.com>
2022-08-26 15:35:49 +0200

    Watch List - update

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,6 +6,7 @@ class BookmarksController < ApplicationController
   def new
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
+    @movies_collection = Movie.all - @list.movies
   end
 
   def create
@@ -22,6 +23,9 @@ class BookmarksController < ApplicationController
   def destroy
     # bookmark DELETE /bookmarks/:id(.:format)
     @bookmark = Bookmark.find(params[:id])
+    # we can say @bookmark.list because in our bookmark.rb we have: belongs_to :list
+    # example for @bookmark.list: <List id: 5, name: "newest recommendations", created_at: "2022-08-25 13:...">
+    # we need to define @list because we use it in the redirect
     @list = @bookmark.list
     # Destroy
     @bookmark.destroy
