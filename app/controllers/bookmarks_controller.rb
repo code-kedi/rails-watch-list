87f9f3b62cdb47240729558f69316075e7c9a2f0
Sonja Peschke <sonjape91@gmail.com>
2022-08-26 12:36:55 +0200

    Watch List - added create and destroy methods for bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -4,22 +4,35 @@ class BookmarksController < ApplicationController
   end
 
   def new
-    @bookmark = Bookmark.new
     @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to bookmark_path(@bookmark), status: :see_other, notice: "Bookmark was successfully created"
+      redirect_to list_path(@list), status: :see_other, notice: "Bookmark was successfully created"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    # bookmark DELETE /bookmarks/:id(.:format)
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    # Destroy
+    @bookmark.destroy
+    # Redirect
+    redirect_to list_path(@list), status: :see_other, notice: "Successfully deleted review"
+  end
+
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    # here I had only :movie first, but it needs to be :movie_id
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
